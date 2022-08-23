a3e384322d7ac6249934a46f80980a62265ef9f2
Tangui Le Pense <29804907+tanguilp@users.noreply.github.com>
2022-08-23 22:23:51 +0300

    Update config to enabled http_cache_store_native clustering

--- config/config.exs
+++ config/config.exs
@@ -17,6 +17,7 @@ config :plug_http_cache_demo, PlugHTTPCacheDemoWeb.Endpoint,
 config :plug_http_cache_demo, :plug_http_cache_opts, store: :http_cache_store_native
 
 config :http_cache_store_native, :memory_limit, 1024 * 1024 * 120
+config :http_cache_store_native, :cluster_enabled, true
 
 # Configure esbuild (the version is required)
 config :esbuild,
