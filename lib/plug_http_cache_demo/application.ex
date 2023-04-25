469db7f19da3c63743526083ce82c7b83ec55e67
Tangui Le Pense <29804907+tanguilp@users.noreply.github.com>
2023-04-25 16:24:38 +0300

    Move telemetry code to own module

--- lib/plug_http_cache_demo/application.ex
+++ lib/plug_http_cache_demo/application.ex
@@ -5,8 +5,6 @@ defmodule PlugHTTPCacheDemo.Application do
 
   use Application
 
-  import Telemetry.Metrics
-
   @topologies [
     docker_compose: [
       strategy: Cluster.Strategy.Gossip
@@ -38,33 +36,11 @@ defmodule PlugHTTPCacheDemo.Application do
   end
 
   defp env_specific_children(:prod) do
-    [{TelemetryMetricsStatsd, metrics: metrics(), host: "statsd_exporter", formatter: :datadog}]
+    [{TelemetryMetricsStatsd, metrics: PlugHTTPCacheDemo.Telemetry.metrics(), host: "statsd_exporter", formatter: :datadog}]
   end
 
   defp env_specific_children(_) do
     []
   end
 
-  defp metrics() do
-    [
-      summary("http_cache.lookup.total_time", unit: :microsecond, tag_values: &add_node/1, tags: [:node]),
-      summary("http_cache.cache.total_time", unit: :microsecond, tag_values: &add_node/1, tags: [:node]),
-      counter("http_cache.cache.total_time", event_name: [:http_cache, :cache_operation], tag_values: &add_node/1, tags: [:node]),
-      counter("http_cache.compress_operation.count", event_name: [:http_cache, :compress_operation], tag_values: &add_node/1, tags: [:node]),
-      counter("http_cache.store.error.count", event_name: [:http_cache, :store, :error], tag_values: &add_node/1, tags: [:node, :reason]),
-      counter("http_cache.decompress_operation.count", event_name: [:http_cache, :decompress_operation], tag_values: &add_node/1, tags: [:node]),
-      counter("http_cache_store_native.object_deleted.count", event_name: [:http_cache_store_native, :object_deleted], tag_values: &add_node/1, tags: [:node, :reason]),
-      last_value("http_cache_store_native.memory.total_mem", unit: :byte, tag_values: &add_node/1, tags: [:node]),
-      last_value("http_cache_store_native.memory.objects_mem", unit: :byte, tag_values: &add_node/1, tags: [:node]),
-      last_value("http_cache_store_native.memory.lru_mem", unit: :byte, tag_values: &add_node/1, tags: [:node]),
-      last_value("http_cache_store_native.memory.objects_count", tag_values: &add_node/1, tags: [:node]),
-      summary("http_cache_store_native.lru_nuker.stop.duration", unit: {:native, :microsecond}, tag_values: &add_node/1, tags: [:node]),
-      summary("http_cache_store_native.expired_lru_entry_sweeper.stop.duration", unit: {:native, :microsecond}, tag_values: &add_node/1, tags: [:node]),
-      summary("http_cache_store_native.expired_resp_sweeper.stop.duration", unit: {:native, :microsecond}, tag_values: &add_node/1, tags: [:node]),
-      counter("plug_http_cache.hit.count", event: [:plug_http_cache, :hit], tag_values: &add_node/1, tags: [:node]),
-      counter("plug_http_cache.miss.count", event: [:plug_http_cache, :miss], tag_values: &add_node/1, tags: [:node])
-    ]
-  end
-
-  defp add_node(tags), do: Map.put(tags, :node, node())
 end
